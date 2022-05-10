2737926259befd3e7226962f1533562967c6a6f1
Apache-Duster <mikelopezca@gmail.com>
2022-05-10 09:16:03 +0200

    objects

---
+++ 01-objects.js
@@ -0,0 +1,14 @@
+const person1 = {
+    firstName: "Martha", 
+    age: 26,
+};
+
+const person2  = {
+    firstName; "Jim",
+    age: 16,
+};
+
+const person3 = {
+    firstName: "Mike",
+    age: 19,
+};
